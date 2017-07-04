cfe787fb2b6e3c44a6c7917c280d7aa11ec2f33f
Tanyayasiuk <t.yasiuk@gmail.com>
2017-07-04 16:11:34 +0300

    dom updated
    now it parses waypoints

--- dz/src/javapvt/FinalProject/Dom.java
+++ dz/src/javapvt/FinalProject/Dom.java
@@ -1,4 +1,4 @@
-package FinalProject;
+package javapvt.FinalProject;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -8,10 +8,10 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Dom {
-
 
     public static Root domParser(String path) {
         Document dom = null;
@@ -33,20 +33,21 @@ public class Dom {
 
         String name = nameNode.getFirstChild().getNodeValue();
         rootXML.setName(nameNode.getFirstChild().getNodeValue());
-        System.out.println("name = " + name);
+        //System.out.println("name = " + name);
 
         NodeList dateNodeList = root.getElementsByTagName("date");
         Node dateNode = dateNodeList.item(0);
 
         String date = dateNode.getFirstChild().getNodeValue();
-        System.out.println("date: " + date);
+        //System.out.println("date: " + date);
         rootXML.setDate(dateNode.getFirstChild().getNodeValue());
 
         NodeList busesNodeList = root.getElementsByTagName("schedule");
         Node busesNode = busesNodeList.item(0);
-        System.out.println("tag 4 = " + busesNode.getNodeName());
+        //System.out.println("tag 4 = " + busesNode.getNodeName());
 
         NodeList elementsNodeList = busesNode.getChildNodes();
+
 
         ArrayList<Bus> schedule = new ArrayList<>();
         rootXML.setSchedule(schedule);
@@ -61,49 +62,67 @@ public class Dom {
 
             Bus bus = new Bus();
 
-            System.out.println("======================================");
+            //System.out.println("======================================");
 
             Element element = (Element) node;
-            //System.out.println("tag = " + node.getNodeName());
 
             // busNum
             NodeList nameElemlist = element.getElementsByTagName("busNumber");
             Element nameElement = (Element) nameElemlist.item(0);
             String busNum = nameElement.getFirstChild().getNodeValue();
-            System.out.println("busNumber : " + busNum);
+            //System.out.println("busNumber : " + busNum);
 
             bus.setBusNumber(Integer.valueOf(busNum));
 
             // dateEnd
             String dateEnd = element.getElementsByTagName("dateEnd").item(0).getTextContent();
-            System.out.println("dateEnd : " + dateEnd);
+            //System.out.println("dateEnd : " + dateEnd);
             bus.setDateEnd(dateEnd);
 
             //dateStart
             String dateStart = element.getElementsByTagName("dateEnd").item(0).getTextContent();
-            System.out.println("dateStart : " + dateStart);
+            //System.out.println("dateStart : " + dateStart);
             bus.setDateStart(dateStart);
 
             // id
             String idBus = element.getElementsByTagName("id").item(0).getTextContent();
-            System.out.println("id : " + idBus);
+            //System.out.println("id : " + idBus);
             bus.setId(Integer.valueOf(idBus));
 
             // location
             String locationEnd = element.getElementsByTagName("locationEnd").item(0).getTextContent();
-            System.out.println("locationEnd : " + locationEnd);
+            //System.out.println("locationEnd : " + locationEnd);
             bus.setLocationEnd(locationEnd);
 
             //locationStart
             String locationStart = element.getElementsByTagName("locationStart").item(0).getTextContent();
-            System.out.println("locationStart : " + locationStart);
+            //System.out.println("locationStart : " + locationStart);
             bus.setLocationStart(locationStart);
 
             //price
             String price = element.getElementsByTagName("price").item(0).getTextContent();
-            System.out.println("price : " + price);
+            //System.out.println("price : " + price);
             bus.setPrice(Integer.valueOf(price));
 
+            //waypoints - seem to work now
+            NodeList e = element.getElementsByTagName("waypoints").item(0).getChildNodes();
+            ArrayList<String> waypoints = new ArrayList<>();
+
+            for (int j = 0; j < e.getLength(); j++){
+
+                Node nodeE = e.item(j);
+
+                if (nodeE.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
+                }
+
+                if (nodeE.getTextContent() != null)
+                    waypoints.add(nodeE.getTextContent());
+
+            }
+
+            //System.out.println(waypoints.toString());
+            bus.setWaypoints(waypoints);
 
             schedule.add(bus);
         }
